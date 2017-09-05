4d7b54b301232d6a67a22fea3104bf3ca7ffcc59
Richard Szemerei <richard.szemerei@socialbakers.com>
2017-09-05 17:33:23 +0200

    Added first version of SBKS Master file.

--- masters/masters-sbks.js
+++ masters/masters-sbks.js
@@ -3,19 +3,45 @@ import {
 } from './utils';
 
 import {
-	IMAGES
+	IMAGES,
+	BACKGROUNDS
 } from './sbks-assets/images';
+
+const PPTX_COLORS = {
+	TEXT1: 'tx1',
+	TEXT2: 'tx2',
+	BACKGROUND1: 'bg1',
+	BACKGROUND2: 'bg2',
+	ACCENT1: 'accent1',
+	ACCENT2: 'accent2',
+	ACCENT3: 'accent3',
+	ACCENT4: 'accent4',
+	ACCENT5: 'accent5',
+	ACCENT6: 'accent6',
+}
 
 const SETTINGS = {
 	LAYOUT: 'LAYOUT_WIDE',
-	MARGIN: 0.4
+	MARGIN: 0.3
 }
 
 const COLORS = {
 	BACKGROUND: 'f1f1f4',
 	DEFAULT_COLOR: 'ff0000',
 	TITLE_COLOR: '00ff00',
-	FOOTER_BACKGROUND: 'f1f1f4'
+	FOOTER_BACKGROUND: 'f1f1f4',
+	SCHEME: {
+		dk1: '2F3744',
+		dk2: 'F3F3F5',
+		lt1: '6B7883',
+		lt2: 'FFFFFF',
+		accent1: '1A3A82',
+		accent2: '38C0C0',
+		accent3: '3B922B',
+		accent4: 'FFCC00',
+		accent5: 'A22323',
+		accent6: '66245F'
+	}
 }
 
 const FONTS = {
@@ -24,187 +50,159 @@ const FONTS = {
 }
 
 const FOOTER_BAR = {
-	y: '92%',
-	h: '8%',
+	y: '91%',
+	h: '9%',
 	SBKS_LOGO: {
-		w: IMAGES.SBKS_LOGO.w / 3,
-		h: IMAGES.SBKS_LOGO.h / 3
+		w: IMAGES.SBKS_LOGO.w / 2.8,
+		h: IMAGES.SBKS_LOGO.h / 2.8
 	}
 }
 
 let _footerBar = [{
 	rect: {
 		x: 0,
-		y: 0,
+		y: FOOTER_BAR.y,
 		w: '100%',
 		h: FOOTER_BAR.h,
 		fill: COLORS.FOOTER_BACKGROUND
 	}
 }, {
 	image: {
-		x: SETTINGS.MARGIN,
-		y: '94%',
+		x: '3%',
+		y: '93%',
 		w: pxToInch(FOOTER_BAR.SBKS_LOGO.w),
 		h: pxToInch(FOOTER_BAR.SBKS_LOGO.h),
 		data: IMAGES.SBKS_LOGO.data
 	}
 }];
 
-const _slideNumber = {
-	x: 0,
-	y: 0,
-	fontSize: FONTS.FOOTER_FONT_SIZE,
-	color: COLORS.DEFAULT_COLOR,
-	align: 'r',
-	valign: 'm',
-}
-
 const MASTER_FILE = {
 	MASTER_SLIDE: {
 		definitions: {
-			title: 'Slide Master',
+			title: 'Socialbakers Slide Master',
 			bkgd: COLORS.BACKGROUND,
 			color: COLORS.DEFAULT_COLOR,
 			margin: SETTINGS.MARGIN,
 			objects: [].concat(_footerBar),
-		slideNumber: _slideNumber,
+			placeholders: [
+				{
+					type: 'title',
+					text: 'Click to edit title',
+					name: 'title',
+					options: {
+						x: '3%',
+						y: '3.5%',
+						w: '94%',
+						h: '7.5%'
+					}
+				}, {
+					type: 'footer',
+					text: 'Click to edit footer',
+					name: 'footer',
+					options: {
+						x: '15%',
+						y: '93%',
+						w: '50%',
+						h: '4%',
+						font_size: 9
+					}
+				}
+			]
 		},
 		styles: {
 			title: {
-				p: {"align":"ctr","bold":true,"bullet":true,"color":"ff0000","font_face":"Impact","font_size":60,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false},
-				list: [{"align":"center","bold":true,"bullet":true,"color":"333333","font_face":"Impact","font_size":50,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false}]
+				list: [{"align":"left","color":PPTX_COLORS.TEXT1,"font_face":FONTS.DEFAULT_FONT_FACE,"font_size":24,"valign":"center"}]
 			},
 			body: {
-				p: {"align":"center","bold":true,"bullet":true,"color":"ff00ff","font_face":"Impact","font_size":40,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false},
-				list: [{"align":"center","bold":true,"bullet":true,"color":"ff00ff","font_face":"Impact","font_size":30,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false}]
+				list: [{"align":"left","color":PPTX_COLORS.TEXT1,"font_face":FONTS.DEFAULT_FONT_FACE,"font_size":16,"valign":"top"}]
 			},
 			other: {
-				p: {"align":"center","bold":true,"bullet":true,"color":"333333","font_face":"Impact","font_size":20,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false},
-				list: [{"align":"center","bold":true,"bullet":true,"color":"aaaaaa","font_face":"Impact","font_size":10,"italic":false,"inset":0,"indentLevel":1,"lineSpacing":10,"margin":10,"underline":false,"valign":"b","rtlMode":false}]
+				list: [{"align":"left","color":PPTX_COLORS.TEXT1,"font_face":FONTS.DEFAULT_FONT_FACE,"font_size":16,"valign":"t"}]
 			}
 		}
 	},
 	INTRO_SLIDE: {
-		title: 'Intro Slide',
-		placeholders: [
-			{
-				type: 'ctrTitle',
-				text: 'Click to edit a Dashboard name (custom font Size and color from placeholder)',
-				name: 'title',
-				options: {
-					x: 0,
-					y: 0,
-					w: '100%',
-					h: '45%',
-					font_size: 20,
-					color: 'ff0000'
-				}
+		title: 'Socialbakers Title Slide',
+		objects: [{
+			image: {
+				x: 0,
+				y: 0,
+				w: '100%',
+				h: '100%',
+				data: BACKGROUNDS.TITLE_SLIDE.data
 			}
-		]
-	},
-	TITLE_SLIDE: {
-		title: 'Title only Layout',
-		slideNumber: _slideNumber,
+		}].concat(_footerBar),
 		placeholders: [
 			{
 				type: 'title',
-				text: 'Click to edit title (styles are inherited from master slide)',
+				text: 'Dashboard Name',
 				name: 'title',
 				options: {
-					x: 0,
+					x: '3%',
 					y: '10%',
-					w: '100%',
-					h: '40%'
+					w: '94%',
+					h: '35%',
+					font_size: 48,
+					color: COLORS.SCHEME.dk2,
+					align: 'center',
+					valign: 'bottom'
 				}
 			}, {
-				type: 'text',
-				text: 'Click to edit text (styles are inherited from master slide)',
-				name: 'text',
+				type: 'subTitle',
+				text: 'Data Range of the Report',
+				name: 'subTitle',
 				options: {
-					x: 0,
-					y: '50%',
-					w: '100%',
-					h: '40%'
+					x: '3%',
+					y: '47%',
+					w: '94%',
+					h: '23%',
+					font_size: 20,
+					color: COLORS.SCHEME.dk2,
+					align: 'center',
+					valign: 'top'
 				}
 			}
 		]
 	},
-	CHART_SLIDE: {
-		title: 'Chart Slide',
-		bkgd: '00FF00',
-		color: COLORS.DEFAULT_COLOR,
-		margin: SETTINGS.MARGIN,
-		placeholders: [{
-			type: 'chart',
-			name: 'chart',
-			options: {
-				x: 2,
-				y: 2,
-				w: 11,
-				h: 5,
+	TITLE_ONLY: {
+		title: 'Socialbakers Section Header',
+		objects: [].concat(_footerBar),
+		placeholders: [
+			{
+				type: 'title',
+				text: 'Click to edit title',
+				name: 'title',
+				options: {
+					x: '3%',
+					y: '11%',
+					w: '94%',
+					h: '37%',
+					font_size: 32,
+					align: 'center',
+					valign: 'bottom'
+				}
 			}
-		}]
+		]
+	},
+	TITLE_AND_CONTENT: {
+		title: 'Socialbakers Title and Content',
+		objects: [].concat(_footerBar),
+		placeholders: [
+			{
+				type: 'content',
+				text: 'Content',
+				name: 'content',
+				options: {
+					x: '3%',
+					y: '13%',
+					w: '94%',
+					h: '75%',
+					valign: 't'
+				}
+			}
+		]
 	}
 }
-
-/*const SLIDES = {
-	MASTER_SLIDE: {
-		DASHBOARD_NAME: {
-			x: pxToInch(180),
-			y: SETTINGS.LAYOUT.height - FOOTER_BAR.h,
-			h: FOOTER_BAR.h,
-			font_face: FONTS.DEFAULT_FONT_FACE,
-			font_size: FONTS.FOOTER_FONT_SIZE,
-			color: COLORS.DEFAULT_COLOR,
-			align: 'l',
-			valign: 'm',
-		}
-	},
-	INTRO_SLIDE: {
-		DASHBOARD_NAME: {
-			x: 0,
-			y: '30%',
-			w: '100%',
-			h: 1.5,
-			align: 'c',
-			valign: 'b',
-			font_size: 24,
-			color: COLORS.TITLE_COLOR,
-		},
-		DATERANGE: {
-			x: 0,
-			y: '50%',
-			w: '100%',
-			h: 1,
-			align: 'c',
-			valign: 't',
-			font_size: 14,
-			color: COLORS.TITLE_COLOR,
-		}
-	},
-	TITLE_SLIDE: {
-		TITLE: {
-			x: 0,
-			y: '30%',
-			w: '100%',
-			h: 1.5,
-			align: 'c',
-			valign: 'b',
-			font_size: 24,
-			color: COLORS.TITLE_COLOR,
-		},
-		SUB_TITLE: {
-			x: 0,
-			y: '50%',
-			w: '100%',
-			h: 1,
-			align: 'c',
-			valign: 't',
-			font_size: 14,
-			color: COLORS.TITLE_COLOR,
-		}
-	}
-}*/
 
 export {
 	SETTINGS,
